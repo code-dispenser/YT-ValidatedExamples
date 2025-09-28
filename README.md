fc83a377f12e52b48f8006a832a22b68f1f3a7b9
Paul Kent <paulandrewkent@hotmail.com>
2025-09-28 14:38:19 +0100

    Updated readme / license

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ValidatedExamples
+<h1>
+<img src="https://github.com/code-dispenser.png" align="center" height="60px" alt="code-dispenser icon" /> code-dispenser
+</h1>
+
+This repository contains the solution made for a YouTube video showing how to use Validated.Core.
+
+YouTube video: TBA
--- ValidatedExamples/Program.cs
+++ ValidatedExamples/Program.cs
@@ -6,15 +6,15 @@ internal class Program
 {
     static async Task Main()
     {
-        //await The_Validated_Type.Run();
+        await The_Validated_Type.Run();
 
-        //await The_MemberValidator_Delegate.Run();
+        await The_MemberValidator_Delegate.Run();
 
-        //await Custom_MemberValidator_Factory.Run();
+        await Custom_MemberValidator_Factory.Run();
 
-        //await The_AndThen_Extension.Run();
+        await The_AndThen_Extension.Run();
 
-        //await The_EntityValidator_Delegate.Run();
+        await The_EntityValidator_Delegate.Run();
 
         await The_ValidationBuilder.Run();
 
