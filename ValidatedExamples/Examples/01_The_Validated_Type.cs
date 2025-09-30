c9e10cc76e82008d59f0c6cbfdc0dc940fc5585f
Paul Kent <paulandrewkent@hotmail.com>
2025-09-30 13:35:16 +0100

    tidy up

--- ValidatedExamples/Examples/01_The_Validated_Type.cs
+++ ValidatedExamples/Examples/01_The_Validated_Type.cs
@@ -9,29 +9,36 @@ public static class The_Validated_Type
         * The Validated<T> is the type that gets returned from all functions/delegates. It is either valid containing the valid value
         * or invalid containing a readonly list of InvalidEntries.
     */ 
-    public static Task Run()
+    public static async Task Run()
     {
         var validResult   = Validated<string>.Valid("john.doe@mail.com");
 
-        var invalidResult = Validated<string>.Invalid(new InvalidEntry(FailureMessage:"Email format is invalid", Path: "Email", PropertyName: "Email", 
-                                                                       DisplayName: "Email Address", Cause: CauseType.Validation));
         /*
-            * To get the value out use either the Match method supplying two functions, one for sad path and one for the happy path as it could be either  
+            * To get the value out use either the Match method supplying two functions, one for the sad path and one for the happy path as it could be either  
         */ 
 
         var theValidValue = validResult.Match(onInvalid: failures => string.Join(", ", failures.Select(f => f.FailureMessage)), onValid: value => value);
-
-        //Or, you can supply a default value to use if the value is invalid via the GetValueOr method which will then return either the valid value or the supplied default value. 
-
+        
+        /*
+            *  Or, you can supply a default value to use if the value is invalid via the GetValueOr method which will then return either the valid value or the supplied default value. 
+        */ 
+        
         var theValueToUse = theValidValue = validResult.GetValueOr("john.doe@secondary-email.com");
+
+        await Console.Out.WriteLineAsync($"Valid result match outputs: {theValidValue} - The value to use outputs: {theValueToUse}\r\n");
 
         /*
             * Lets do the same for the invalid result 
-        */ 
+        */
+
+        var invalidResult = Validated<string>.Invalid(new InvalidEntry(FailureMessage: "Email format is invalid", Path: "Email", PropertyName: "Email",
+                                                                       DisplayName: "Email Address", Cause: CauseType.Validation));
 
         var invalidValue = invalidResult.Match(onInvalid: failures => string.Join(", ", failures.Select(f => f.FailureMessage)), onValid: value => value);
 
-        var defaultValue = theValidValue = validResult.GetValueOr("john.doe@secondary-email.com");
+        var defaultValue = invalidResult.GetValueOr("john.doe@secondary-email.com");
+
+        await Console.Out.WriteLineAsync($"Invalid result match outputs: {invalidValue} - The value to use outputs: {defaultValue}\r\n");
 
         /*
             * You can also check the IsValid or IsInvalid properties to see the validity of the contents. 
@@ -39,7 +46,5 @@ public static class The_Validated_Type
 
         var isValid   = validResult.IsValid;
         var isInvalid = invalidResult.IsInvalid;
-
-        return Task.CompletedTask;
     }
 }
--- ValidatedExamples/Program.cs
+++ ValidatedExamples/Program.cs
@@ -8,15 +8,15 @@ internal class Program
     {
         await The_Validated_Type.Run();
 
-        await The_MemberValidator_Delegate.Run();
+        //await The_MemberValidator_Delegate.Run();
 
-        await Custom_MemberValidator_Factory.Run();
+        //await Custom_MemberValidator_Factory.Run();
 
-        await The_AndThen_Extension.Run();
+        //await The_AndThen_Extension.Run();
 
-        await The_EntityValidator_Delegate.Run();
+        //await The_EntityValidator_Delegate.Run();
 
-        await The_ValidationBuilder.Run();
+        //await The_ValidationBuilder.Run();
 
         Console.ReadLine();
     }
